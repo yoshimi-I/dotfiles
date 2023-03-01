2d76a1358e7d5f0d167cb481e3ea81d2fd2bb154
yoshimi-I <iwatsuki.443@outlook.com>
2023-03-01 21:50:33 +0900

    fix: homebrewのインストール方法を一部修正

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 if [ "$(uname -m)" = "arm64" ] ; then
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/${USER}/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)" > /dev/null
 fi
 
