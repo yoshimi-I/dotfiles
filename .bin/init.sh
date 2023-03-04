0d65ebce063cd55d6eb0a81177ab5c82807b3604
yoshimi-I <iwatsuki.443@outlook.com>
2023-03-04 18:41:47 +0900

    fix: brewのコマンドが使えないエラーを解消

--- .bin/init.sh
+++ .bin/init.sh
@@ -3,8 +3,7 @@
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 if [ "$(uname -m)" = "arm64" ] ; then
-  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/${USER}/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/${USER}/.zprofile && eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Install xcode
