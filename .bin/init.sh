68158d74bfd2f0dbd09fcb8e751069941fe90794
yoshimi-I <iwatsuki.443@outlook.com>
2023-03-04 19:57:07 +0900

    fix: shellを変更

--- .bin/init.sh
+++ .bin/init.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Install brew
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 if [ "$(uname -m)" = "arm64" ] ; then
   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/${USER}/.zprofile && eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
