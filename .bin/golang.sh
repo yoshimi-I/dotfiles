1f8ddfedd435cec97fbe5bdb2ed3ccd452615804
yoshimi-I <iwatsuki.443@outlook.com>
2023-03-20 23:10:17 +0900

    add: Goenvインストールの処理の記載

--- .bin/golang.sh
+++ .bin/golang.sh
@@ -1,2 +1,8 @@
 #!/bin/zsh
-
+if ! command -v goenv &> /dev/null; then
+    echo "goenv, installing now..."
+    git clone https://github.com/syndbg/goenv.git ~/.goenv
+    source ~/.zshrc
+else
+    echo "goenv is already installed"
+fi
