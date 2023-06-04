7846f0b5e7c5f5d63243884868c33ae718ec6b08
yoshimi-I <iwatsuki.443@outlook.com>
2023-06-05 00:07:03 +0900

    add: sharship用の設定ファイル一覧

--- .bin/link.sh
+++ .bin/link.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)" #/bin
 
 
+echo "Create a symbolic link for dotfiles..."
 for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
@@ -10,5 +11,10 @@ for dotfile in "${SCRIPT_DIR}"/.??* ; do
 
     ln -fnsv "$dotfile" "$HOME"
 done
+
+echo "Create a symbolic starship setting..."
+STARSHIP_CONFIG_DIR="$(cd "$(SCRIPT_DIR)" && pwd)"/starship"
+
+ln -fnsv "${STARSHIP_CONFIG_DIR}/starship.toml" "$HOME/.config"
 
 source ~/.zshrc
