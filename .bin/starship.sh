588ce2989641e8c31560f7858d80736258ba887e
yoshimi-I <iwatsuki.443@outlook.com>
2023-10-05 02:06:08 +0900

    fix: 簡単にした

--- .bin/starship.sh
+++ .bin/starship.sh
@@ -1,12 +1,4 @@
 #!/bin/zsh
 
-echo "Install "
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)" #/bin
-
-echo "Create a symbolic starship setting..."
-STARSHIP_CONFIG_DIR="$(dirname "$CURRENT_DIR")/starship"
-
-ln -fnsv "${STARSHIP_CONFIG_DIR}/starship.toml" "$HOME/.config"
-
-source ~/.zshrc
+echo "Set up Starship "
+cp ~/dotfiles/starship/starship.toml ~/.config
