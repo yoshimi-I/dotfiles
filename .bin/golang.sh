db3e35e3876b31e710917758997f186838b78093
yoshimi-I <iwatsuki.443@outlook.com>
2024-03-05 19:49:27 +0900

    fix: 不必要ファイルの削除

--- .bin/golang.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-if ! command -v goenv &> /dev/null; then
-    echo "goenv, installing now..."
-    git clone https://github.com/syndbg/goenv.git ~/.goenv
-    source ~/.zshrc
-else
-    echo "goenv is already installed"
-fi
